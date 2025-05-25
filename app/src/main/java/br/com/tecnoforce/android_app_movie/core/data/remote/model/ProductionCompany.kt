da5fd8d9b09e7ce60c0dd3a5aeb677a8a242873f
Ricardo Cavalcanti <ricardocavalcanti@outlook.com>
2025-05-25 12:31:11 -0300

    refactor: organizar codigo

--- app/src/main/java/br/com/tecnoforce/android_app_movie/core/data/remote/model/ProductionCompany.kt
+++ app/src/main/java/br/com/tecnoforce/android_app_movie/core/data/remote/model/ProductionCompany.kt
@@ -3,7 +3,6 @@ package br.com.tecnoforce.android_app_movie.core.data.remote.model
 import com.google.gson.annotations.SerializedName
 
 data class ProductionCompany(
-
     @SerializedName("id")
     val id: Int,
 
